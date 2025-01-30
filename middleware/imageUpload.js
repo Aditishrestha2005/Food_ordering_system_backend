4bddfd7dd86df41a2b637e107ada65bc7e40fb49
aditi <adishrestha2005@gmail.com>
2025-01-30 14:47:46 +0545

    productController,uploadfolder

--- middleware/imageUpload.js
+++ middleware/imageUpload.js
@@ -3,24 +3,27 @@ const path = require('path');
 
 // Configure storage
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/'); // Directory where files will be stored
-        cb(null, `${Date.now()}-${file.originalname}`); // Ensure unique filenames
-    }
+  destination: (req, file, cb) => {
+    cb(null, 'uploads'); // Directory where files will be stored
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`); // Ensure unique filenames
+  }
 });
 
 // File filter to allow only images
 const fileFilter = (req, file, cb) => {
-    if (file.mimetype.startsWith('image/')) {
-        cb(null, true); // Accept the file
-    } else {
-        cb(new Error('Only image files are allowed!'), false); // Reject the file
-    }
+  if (file.mimetype.startsWith('image/')) {
+    cb(null, true); // Accept file
+  } else {
+    cb(new Error('Only image files are allowed!'), false); // Reject file
+  }
 };
 
+// Set up multer
 const upload = multer({
-    storage: storage,
-    fileFilter: fileFilter
+  storage,
+  fileFilter
 });
 
 module.exports = upload;
